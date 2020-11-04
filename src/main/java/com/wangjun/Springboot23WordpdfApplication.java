bd976b362892812aed8a2000099d38296598a496
xi_nian <1617171230@qq.com>
2020-11-05 01:07:00 +0800

    项目搭建-0.10

---
+++ src/main/java/com/wangjun/Springboot23WordpdfApplication.java
@@ -0,0 +1,13 @@
+package com.wangjun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot23WordpdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot23WordpdfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wangjun/Springboot23WordpdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangjun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot23WordpdfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
